305c5220eb11e3243bc035a8abeb6acabf6efce2
Henry Arjet <henry.arjet@smoothstack.com>
2021-08-10 20:52:48 -0500

    Week 2 assignment 1?

---
+++ nptest.py
@@ -0,0 +1,9 @@
+import numpy as np
+print (np.zeros(10))
+print (np.ones(10))
+print (np.full((10),5))
+print (np.arange(10, 51))
+print (np.arange(10, 51, 2))
+print (np.arange(0, 9).reshape(3,3))
+print (np.eye(3))
+print (np.random.rand())
